5cf2595ba7b2fcd31f49de2144fe93f1e2cba853
chananan47 <namayura47@gmail.com>
2021-11-26 10:01:36 +0700

    update -v

---
+++ function1.php
@@ -0,0 +1,32 @@
+<?php
+//Function 
+
+function add(){
+    $a = 10;
+    $b = 5;
+    $add = $a + $b;
+    echo "This is a Function add =" .$add."<br />";
+}
+
+function sub(){
+    $a = 10;
+    $b = 5;
+    $sub = $a - $b;
+    echo "This is a Function Sub =" .$sub."<br />";
+}
+function division(){
+    $a = 10;
+    $b = 5;
+    $division = $a/$b;
+    echo "This is a Function Division =" .$division."<br />";
+}
+function multiply(){
+    $a = 10;
+    $b = 5;
+    $multip = $a*$b;
+    echo "This is a Function Multiply =" .$multip."<br />";
+}
+add();
+sub();
+division();
+multiply();
